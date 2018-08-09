812b1a553576d67197240b6b46bdcbe224724b94
animatedmax <mhufnagel@pivotal.io>
2018-08-09 12:13:32 -0700

    Fix obvious typos

--- layout/source/javascripts/vendor/mermaid.js
+++ layout/source/javascripts/vendor/mermaid.js
@@ -27528,8 +27528,8 @@ module.exports=require(53)
 		'\'': '&#x27;',
 		'<': '&lt;',
 		// See https://mathiasbynens.be/notes/ambiguous-ampersands: in HTML, the
-		// following is not strictly necessary unless it’s part of a tag or an
-		// unquoted attribute value. We’re only escaping it to support those
+		// following is not strictly necessary unless it's part of a tag or an
+		// unquoted attribute value. We're only escaping it to support those
 		// situations, and for XML support.
 		'>': '&gt;',
 		// In Internet Explorer ≤ 8, the backtick character can be used
@@ -27677,7 +27677,7 @@ module.exports=require(53)
 				return '&' + encodeMap[string] + ';';
 			});
 		} else if (!allowUnsafeSymbols) {
-			// Encode `<>"'&` using hexadecimal escapes, now that they’re not handled
+			// Encode `<>"'&` using hexadecimal escapes, now that they're not handled
 			// using named character references.
 			string = string.replace(regexEscape, hexEscape);
 		}
@@ -27748,7 +27748,7 @@ module.exports=require(53)
 					return $0;
 				}
 			}
-			// If we’re still here, it’s a legacy reference for sure. No need for an
+			// If we're still here, it's a legacy reference for sure. No need for an
 			// extra `if` check.
 			// Decode named character references without trailing `;`, e.g. `&amp`
 			// This is only a parse error if it gets converted to `&`, or if it is
